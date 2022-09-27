a2ca5d033be0c157303f6c2086a8e4c777137988
Jan Zaborowicz~ <janek.zaba05@gmail.com>
2022-09-27 17:10:41 +0200

    ligth

--- 27.9.2022/main.js
+++ 27.9.2022/main.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const linijka = this.getElementById('line');
-    const button = document.querySelector('button')
+    const button = this.getElementById('textBtn')
+    const light = this.getElementById('light')
+    const buttonLight = this.getElementById('lightBtn')
 
+    button.addEventListener('click', function() {
 
-button.addEventListener('click', function() {
+        linijka.classList.toggle('red');
 
-    linijka.classList.toggle('red');
+    })
 
-})
+    buttonLight.addEventListener('click', function() {
 
-   
+        light.classList.toggle('lightOFF');
+        light.classList.toggle('lightON');
 
+    })
 })
